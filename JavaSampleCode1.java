99c515939996f77e51c4d1eba585459f6f841d6b
Makoto Tanabe <xekid78@outlook.com>
2017-07-17 16:18:07 +0900

    Delete JavaSampleCode1.java

--- JavaSampleCode1.java
+++
@@ -1,9 +0,0 @@
-class Main {
-  public static void main(String[] args) throws Exception {
-    for(int seireki = 1926; seireki <= 1935; seireki++) {
-      System.out.print("西暦" + seireki + "年は");
-      int syowa = seireki - 1925;
-      System.out.println("昭和" + syowa + "年です");
-    }
-  }
-}
