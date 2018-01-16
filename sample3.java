90ae84bcf96107b91c9aa47e0475cc1c4ddfcc06
Makoto Tanabe <xekid78@outlook.com>
2018-01-16 16:49:56 +0900

    Add files via upload

---
+++ sample3.java
@@ -0,0 +1,9 @@
+public class sample3 {
+  public static void main(String[] args) {
+    for(int seireki = 1926; seireki <= 1935; seireki++) {
+      System.out.print("西暦" + seireki + "年は");
+      int syowa = seireki - 1925;
+      System.out.println("昭和" + syowa + "年です");
+    }
+  }
+}
