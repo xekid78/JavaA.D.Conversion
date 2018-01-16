1d4963b374dfe3341f0ca70d7757e772704d9157
Makoto Tanabe <xekid78@outlook.com>
2018-01-16 21:09:34 +0900

    Update sample3.java

--- sample3.java
+++ sample3.java
@@ -1,4 +1,10 @@
+/**
+ *
+ * @author xekid78
+ *
+ */
 public class sample3 {
+  
   public static void main(String[] args) {
     for(int seireki = 1926; seireki <= 1935; seireki++) {
       System.out.print("西暦" + seireki + "年は");
